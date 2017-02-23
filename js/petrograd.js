11db9af99319e77524995185f56b0812449a15b4
Cathrine Juul Graugaard <cathrinejuulgraugaard@persocomcchi.kea.local>
2017-02-23 13:43:42 +0100

    remove udsalgs tekst

--- js/petrograd.js
+++ js/petrograd.js
@@ -4,7 +4,6 @@ function sidenVises() {
     console.log("Siden vises");
     //læs produktliste
     $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
-    visProdukt();
 }
 
 function visProduktListe(listen) {
@@ -17,6 +16,28 @@ function visProdukt(produkt) {
     //klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     //indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+    var rabatpris = produkt.pris - (produkt.pris = produkt.rabatsats / 100);
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
+
+    klon.querySelector(".data_billede").src = "/imgs/small/" + produkt.billede + "-sm.jpg";
+
+
+    if (produkt.udsolgt == false) {
+        //produktet er ikke udsolgt
+        //Udsolgt teskt skal fjernes
+        var udsolgttekst = klon.querySelector(".udsolgttekst");
+        udsolgttekst.parentNode.removeChild(udsolgttekst);
+
+    } else {
+        klon.querySelector(".pris").classList.add("udsolgt");
+    }
+
+
+
     //append klon til produkt_liste
+
     document.querySelector(".produktliste").appendChild(klon);
 }
